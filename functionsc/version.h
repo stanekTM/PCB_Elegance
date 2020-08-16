321a9e0b1056927fdd09221b4f487f9e4829a6f1
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-08-17 01:14:48 +0200

    delete

--- functionsc/version.h
+++ functionsc/version.h
@@ -1,8 +1,8 @@
 // PCB Elegance release version
 #define VER_VERSION				352
 #define VER_VERSION_STR			"3.52"
-#define VER_DATE_STR			"29.01.2020"
+#define VER_DATE_STR			"17.08.2020"
 // for .rc files
-#define VER_FILEVERSION			3,52,0,0
-#define VER_FILEVERSION_STR		"3,52,0,0"
-#define VER_BUILD 0
+#define VER_FILEVERSION			3,52,1,0
+#define VER_FILEVERSION_STR		"3,52,1,0"
+#define VER_BUILD 1
